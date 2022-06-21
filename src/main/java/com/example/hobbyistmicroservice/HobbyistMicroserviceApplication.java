de17c2a78eb2d52eafb84f0a621d84c95701963c
George <sanchez28112001@gmail.com>
2022-06-20 22:36:26 -0500

    initialized project

---
+++ src/main/java/com/example/hobbyistmicroservice/HobbyistMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.hobbyistmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyistMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyistMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hobbyistmicroservice/HobbyistMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hobbyistmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyistMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
